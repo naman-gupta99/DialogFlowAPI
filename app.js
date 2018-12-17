cb94db9389702ae71570786659c1d1a19f800326
naman-gupta99 <kungfunaman@gmail.com>
2018-12-17 13:54:08 +0530

    v1.0 Complete

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", "https://console.dialogflow.com/api/interactions/conversations?page=1&perPage=50&lang=en", true);
+xmlHttp.open( "GET", "https://cors-anywhere.herokuapp.com/https://console.dialogflow.com/api/interactions/conversations?page=1&perPage=50&lang=en", true);
 xmlHttp.setRequestHeader("Authorization", "Bearer 25745c6c0d0d49bcabd884367a2200c0");
 xmlHttp.setRequestHeader("Content-Type", "application/json");
-xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "https://naman-gupta99.github.io/DialogFlowAPI/");
-xmlHttp.withCredentials = true;
+// xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "https://naman-gupta99.github.io/DialogFlowAPI/");
 xmlHttp.send();
-document.getElementById('data').innerHTML = xmlHttp.responseText;
+xmlHttp.onreadystatechange = function() {
+  if(this.readyState == 4 && this.status == 200) {
+    console.log(xmlHttp.responseText);
+    document.getElementById('data').innerHTML = xmlHttp.responseText;
+  }
+}
+console.log("hello");
